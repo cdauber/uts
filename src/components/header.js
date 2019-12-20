09ee666fb006893d3cd2a97f5d455b3410309b10
Carson Dauber <cdauber@cs.washington.edu>
2019-12-19 23:12:53 -0800

    Add Basic Program

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default () => (
+  <header>
+    <a id="logo" href="/">
+      UTS
+    </a>
+    <nav>
+      <ul>
+        <li class="first">
+          <a class="first" href="/profile" target="_blank">
+            About
+          </a>
+        </li>
+        <li class="">
+          <a class="" href="/works" target="_blank">
+            Works
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
